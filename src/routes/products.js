ab358826caf1b0e4763836217bea1df0558db0cf
aguilarL3 <aguilarleandro33@gmail.com>
2022-07-04 22:43:01 -0300

    modificando ultimos archivos ejs

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsControllers')
 
-/*router.get('/product-detail/:id', productsController.productDetail)
-router.get('/cart', productsController.cart)*/
+/*router.get('/product-detail/:id', productsController.productDetail)*/
+router.get('/cart', productsController.cart)
 
 router.get('/list-product', productsController.productList)
 router.get('/form-product', productsController.productForm)
